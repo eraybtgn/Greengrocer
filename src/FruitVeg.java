fb34b08b7e210c3ab0748a5f0205d71b181681fb
Eray <86014603+eraybtgn@users.noreply.github.com>
2022-11-04 17:23:49 +0300

    Update FruitVeg.java

--- src/FruitVeg.java
+++ src/FruitVeg.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class FruitVeg {
+import java.util.Scanner;
+
+public class FruitVeg {
+    public double apple;
+    public double pear;
+    public double tomatoes;
+    public double banana;
+    public double aubergine;
+    public int weigh;
+
+    public FruitVeg() {
+
+    }
+
+
+        public double howMuch() {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Kaç kilo elma aldınız.");
+            weigh = scan.nextInt();
+            apple=3.67*weigh;
+            System.out.println("Kaç kilo armut aldınız.");
+            weigh = scan.nextInt();
+            pear=2.14*weigh;
+            System.out.println("Kaç kilo domates aldınız.");
+            weigh=scan.nextInt();
+            tomatoes=1.11*weigh;
+            System.out.println("Kaç kilo muz aldnız.");
+            weigh=scan.nextInt();
+            banana=0.95*weigh;
+            System.out.println("Kaç kilo patlıcan aldınız.");
+            weigh=scan.nextInt();
+            aubergine=5.00*weigh;
+
+
+            return weigh;
+
+        }
+
+    @Override
+    public String toString() {
+
+        return "Liste :" +
+                "Elma='" + apple + '\'' +
+                ", Armut='" + pear + '\'' +
+                ", Domates=" + tomatoes +
+                ", Muz=" + banana +
+                ", Patlıcan=" + aubergine +
+                '}';
+    }
 }
