c64f36f075647e85203c908f2609faa2db33cce2
Eray <86014603+eraybtgn@users.noreply.github.com>
2022-11-04 17:24:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+    FruitVeg f1= new FruitVeg();
+    f1.howMuch();
+        System.out.println(f1);
+    }
 }
